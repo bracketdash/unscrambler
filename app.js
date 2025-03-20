ad844ccdcb0e8821c08753c1594ec947ebb57571
Michael Hatch <michaelhatch@me.com>
2025-03-20 07:43:46 -0700

    use more compressed trie

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ let trie = {};
 
 function getTrie(compressed) {
   let decompressed = compressed;
+  decompressed = decompressed.replace(/([A-Z])/g, (c) => c.toLowerCase() + "$");
   decompressed = decompressed.replace(/([a-z])/g, '"$1":{');
+  decompressed = decompressed.replace(/([0-9]+)/g, "$1,").slice(0, -1);
+  decompressed = decompressed.replace(/\$([^0-9])/g, "$,$1");
   const getEndBrackets = (c) => "}".repeat(parseInt(c, 10));
   decompressed = decompressed.replace(/([0-9]+)/g, getEndBrackets);
   decompressed = decompressed.replaceAll("$", '"$":1');
